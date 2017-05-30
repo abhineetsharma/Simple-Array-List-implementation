00e57524d62e321d11f9efa43aacdd7257d66b7e
Abhineet Sharma <asharm28@binghamton.edu>
2017-05-30 19:47:22 -0400

    eclipse check in

--- myArrayList/src/myArrayList/MyArrayList.java
+++ myArrayList/src/myArrayList/MyArrayList.java
@@ -1,4 +1,4 @@
-package myArrayList;
+//package myArrayList;
 
 import java.util.ArrayList;
 
@@ -28,30 +28,46 @@ class MyArrayList{
 			}
 			
 		}
-		this.array[count++]= newValue;
+		this.array[this.count++]= newValue;
+
+		Array.sort(this.array);
 	}
 	public void removeValue(int value){
-		for(int i =0 ; i<count;i++){
-			
+		int counter = -1;
+		boolean flag = false;
+
+		for(int i =0 ; i<this.count;i++){
+			if(flag){
+				this.array[i-1] = this.array[i];
+			}
+			else if(value == this.array[i]){
+				counter = i;
+				flag = true;
+			}
 		}
+		if(counter>-1)this.count--;
 	}
 	public void indexOf(int value){}
 	public void sum(){}
 	public void size(){}
-	// public void display(){
-	// 	System.out.println();
-	// 	for(int i=0;i<count;i++){
-	// 		System.out.print(this.array[i]+" ");
-	// 	}
-	// }
+	public void display(){
+		System.out.println();
+		for(int i=0;i<this.count;i++){
+			System.out.print(this.array[i]+" ");
+		}
+	}
 
-	// public static void main(String [] args){
-	// 	MyArrayList A = new MyArrayList();
+	public static void main(String [] args){
+		MyArrayList A = new MyArrayList();
 
-	// 	for(int i =0;i< 1000;i++){
-	// 		A.insertSorted(i);
-	// 	}
-	// 	A.display();
-	// }
+		for(int i =0;i< 76;i++){
+			A.insertSorted(i);
+		}
+		A.removeValue(10);
+		A.removeValue(10);
+		A.removeValue(100);
+		A.removeValue(8);
+		A.display();
+	}
 	//public void toString(){}
 }
